bf1734ecbf309d9c9e258ae718ada2bbf57b342e
Bawiloussim <bawiloussimngbabou1@gmail.com>
2025-08-17 15:24:26 +0000

    dotenv confired

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 export const fetchBlogs = () => API.get('/blogs');
 export const fetchBlog = (id) => API.get(`/blogs/${id}`);
 export const createBlog = (data) => API.post('/blogs', data);
 export const updateBlog = (id, data) => API.put(`/blogs/${id}`, data);
 export const deleteBlog = (id) => API.delete(`/blogs/${id}`);
+
+export default API;
