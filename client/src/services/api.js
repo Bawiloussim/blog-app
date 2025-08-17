f37c0a264038dd71a0ab0c5267f2fc1584ebce70
Bawiloussim <bawiloussimngbabou1@gmail.com>
2025-08-17 16:19:54 +0000

    api front

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL, withCredentials: true});
+const API = axios.create({ 
+    baseURL: import.meta.env.VITE_API_URL,
+});
 
 export const fetchBlogs = () => API.get('/blogs');
 export const fetchBlog = (id) => API.get(`/blogs/${id}`);
